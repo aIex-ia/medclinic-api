7ee15c0798fcb6d372e65924ce51debc12bf70dd
Alexsandra Oliveira <ale.niffer@gmail.com>
2026-09-14 20:22:26 -0300

    refactor: tipa o corpo da requisicao usando os DTOs para evitar tipos genericos

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,10 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
 import { AuthService } from '../services/AuthService';
+import { CreateUserDTO, LoginDTO } from '../dtos/UserDTOs';
 
 const authService = new AuthService();
 
 export class AuthController {
-  async register(req: Request, res: Response, next: NextFunction) {
+  async register(req: Request<{}, {}, CreateUserDTO>, res: Response, next: NextFunction) {
     try {
       const user = await authService.register(req.body);
       return res.status(201).json(user);
@@ -13,7 +14,7 @@ export class AuthController {
     }
   }
 
-  async login(req: Request, res: Response, next: NextFunction) {
+  async login(req: Request<{}, {}, LoginDTO>, res: Response, next: NextFunction) {
     try {
       const result = await authService.login(req.body);
       return res.status(200).json(result);
